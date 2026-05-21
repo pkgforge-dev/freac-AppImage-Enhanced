d97917f00d882cb91cb3eff5042d65d9d8133863
Link4Electronics <link4electronics@gmail.com>
2026-05-21 16:22:45 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q freac | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.hook"
+export ADD_HOOKS="self-updater.hook:wayland-is-broken.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/128x128/apps/org.freac.freac.png
 export DESKTOP=/usr/share/applications/org.freac.freac.desktop
